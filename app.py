da7666c538d385209b050f70fbe78e359e877358
arai <arailymkaliyeva.m@gmail.com>
2024-02-10 13:19:42 +0100

    I started on my website

--- app.py
+++ app.py
@@ -3,13 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route('/')
+@app.route('/<name>')
 def hello(name=None):
     return render_template('index.html', name=name)
 
